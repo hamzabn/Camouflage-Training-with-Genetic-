7782f2c150a9f8a5e852c2bbcc5c6cedcfbfae2e
hamza <hamza.ben-aissia@talan.com>
2022-06-16 20:10:35 +0100

    ML

---
+++ Assets/Script/DNA.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour
+{
+    //Gene Colors
+    public float r;
+    public float g;
+    public float b;
+    public float s;
+    bool dead = false;
+    public float timeToDie = 0;
+    SpriteRenderer sRenderer;
+    Collider2D sCollider;
+
+    void OnMouseDown(){
+
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        Debug.Log("Dead At :" + timeToDie);
+        sRenderer.enabled = false;
+        sCollider.enabled = false;
+    
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+        sCollider = GetComponent<Collider2D>();
+        sRenderer.color = new Color(r,g,b);
+        this.transform.localScale= new Vector3(s,s,s);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
